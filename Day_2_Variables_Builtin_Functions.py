7242d37541a67507ffe3844b9cc393e8df3116b7
manojsqa <114326543+manojsqa@users.noreply.github.com>
2026-05-31 17:09:17 +0530

    Correct variable names and enhance output formatting
    Fixed variable name typos and improved print statements.

--- Day_2_Variables_Builtin_Functions.py
+++ Day_2_Variables_Builtin_Functions.py
@@ -17,3 +17,38 @@ person_info = {'first_name': 'Manoj', 'last_name': 'Kumar', 'country': 'INDIA', 
 print('first_name: ', first_name)
 print('last_name: ', last_Name)
 print('length of first_name : ', len(first_name))
+print('length of last name: ', len(last_Name))
+print('country: ', country)
+print('state: ', state)
+print('city: ', city)
+print('age: ', age)
+print('maried: ', is_married)
+print('skills: ', skills)
+print('person_information: ', person_info)
+
+
+# Declaring multiplae variables in a single line
+
+
+first_name, last_Name, age, city, state, country, is_married = 'Manoj', 'Kumar', 30, 'Bengaluru', 'Karnataka', 'India', 'True' 
+
+print('first name: ', first_name) 
+print('length_of_first_name: ' , len(fist_name))
+
+print('last_name: ', last_Name)
+print('length_of_last_name: ' , len(last_Name))
+print('age: ', age)
+print('city: ', city)
+print('state: ', state)
+print('Country: ',  country)
+print('Married: ', is_married)
+
+
+
+
+
+
+
+
+
+
