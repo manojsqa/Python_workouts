e29ae28e1d1974f0380290739501c62a608717e6
manojsqa <114326543+manojsqa@users.noreply.github.com>
2026-05-31 16:25:29 +0530

    Initialize personal information variables
    Added variables for personal information and skills.

--- Day_2_Variables_Builtin_Functions.py
+++ Day_2_Variables_Builtin_Functions.py
@@ -1,1 +1,19 @@
+# Day_2:- Variables and Built in Functions
 
+first_name = "Manoj"          # assignng a variable
+last_Name = "Kumar"
+country = "INDIA"
+state = "Karnataka"
+city = "Bengaluru"
+skills = ["python", "Virtualisation", "RAID", "Server_Validation", "Silicon_Validation"]
+age = 30
+is_married = True
+
+person_info = {'first_name': 'Manoj', 'last_name': 'Kumar', 'country': 'INDIA', 'state': 'Karnataka', 'city': 'Bengaluru'}     # a person_Information 
+
+
+# printing the values stored in the variables
+
+print('first_name: ', first_name)
+print('last_name: ', last_Name)
+print('length of first_name : ', len(first_name))
