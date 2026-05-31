6ac0daf4926a7ecc7537fe799babf6323a6ee775
manojsqa <114326543+manojsqa@users.noreply.github.com>
2026-05-31 14:27:13 +0530

    Day 1 introduction with examples and comments
    Added various print statements to demonstrate basic operations and data types in Python.

--- Day_1_introduction.py
+++ Day_1_introduction.py
@@ -1,1 +1,30 @@
+# Python Introduction
+# Day_1 out of Day_30
+print(1/30)
+print(1//30)
+print(1%30)
+
+print("hello world") # command to print hello word
+print(3+10) # addition(+)
+len("hello world!") # command to check how many letters are in "Hello world!"
+print(2-3-1) # substraction(-)
+print(2*9) # Multiplication(*)
+print(2-3) # subtraction(-)
+print(3-2) # subtraction(-)
+print(3/2) # Division(/)
+print(3//2) # Floor Division operator(//)
+print(3**2) # exponential (**)
+print(9+4) # addition
+print(1%30) # modulus (%)
+
+
+# Checking Data_Types
+
+print(type(89677))             # int / Integer
+print(type("hello world!"))    # str / string
+print(type(3.142))             # float
+print(6+5j)                    # complex
+print(type("MaNa")             # str / string
+print(type([1,2,3,4,5,6,7,8,9,19]))  # list
+print(type({1,2,3,4,5,6,7,8,9})) 
 
