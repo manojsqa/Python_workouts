63e6569f1d38296afcf0b0150b4e393e8333e145
manojsqa <114326543+manojsqa@users.noreply.github.com>
2026-06-16 20:30:14 +0530

    Update Day_3_Operators.py

--- Day_3_Operators.py
+++ Day_3_Operators.py
@@ -1,1 +1,31 @@
 
+
+# Arithmatic Operators
+print('addition: ', 1+9)
+print('substraction; ', 9-1)
+print('substraction; ', 1-9)
+print('Multiplication: ', 8*8)
+
+# division operation with output as a floating number 
+
+print("Division: ", 7/4)
+
+# division operation with output as a whole number(floating numbers won't appear here)
+
+print("Division: ", 7//4)
+
+print('Modulus: ", 3%2) #it gives the remainder
+
+print("exponential: ", 4**9)
+
+print("division without the remainder: ', 6//4)
+      
+
+
+
+
+
+
+
+
+      
