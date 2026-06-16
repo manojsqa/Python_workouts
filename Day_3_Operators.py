463dbadb7ad623f040cd9b12116581116012f557
manojsqa <114326543+manojsqa@users.noreply.github.com>
2026-06-16 20:36:42 +0530

    Update Day_3_Operators.py

--- Day_3_Operators.py
+++ Day_3_Operators.py
@@ -16,11 +16,20 @@ print("Division: ", 7//4)
 
 print('Modulus: ", 3%2) #it gives the remainder
 
-print("exponential: ", 4**9)
+print("exponential: ", 4**9) # it means 4*4*4
 
 print("division without the remainder: ', 6//4)
       
 
+# floating numbers
+print('Floating number, PI: ', 3.142)
+print("Floating number, Gravity: ", 9.8)
+
+
+# Complex numbers
+
+print("complex number: ', 1+4j)
+print("multiplying complex number: ', (9+5j) * (4+4j) )
 
 
 
